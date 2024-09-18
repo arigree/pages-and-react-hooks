1fd97e71dd4a40fb00017e435167dfdafd5cbc4f
arigree <arigree@iu.edu>
2024-09-18 10:39:11 -0400

    Added content to home page, fixed pokemon page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,36 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import { useState } from "react";
 
 export default function Home() {
+  // let score = 0;
+  // const scoreState = useState();
+  const [score, setScore] = useState(1);
+  // console.log(scoreState);
+
+  function addToScore() {
+    // score++;
+    // setScore(score + 1);
+    // console.log(score);
+    setScore(function (value) {
+      return value + 1;
+    });
+  }
+
+  // console.log(score);
+
+  function doublePoints() {
+    addToScore();
+    addToScore();
+  }
+
   return (
     <main>
       <h1>Home</h1>
+      <div>
+        <button onClick={addToScore}>Add 1</button>
+        <button onClick={doublePoints}>Add 2</button>
+        <p>Score: {score}</p>
+      </div>
     </main>
   );
 }
