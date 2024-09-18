d52b21b8ad6d98f99ce485b3299c00d10f7c9de2
arigree <arigree@iu.edu>
2024-09-18 09:51:03 -0400

    Added nav and nav styling

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      
+      <body>
+      <nav>
+<ul>
+<li>
+  <a href='/'>Home</a>
+</li>
+<li>
+  <a href='/'>Pokemon</a>
+</li>
+
+</ul>
+
+      </nav>
+        {children}</body>
     </html>
   );
 }
